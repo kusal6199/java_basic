572c07cd49d1413bc38a4a32ccb59123527da835
kusal6199 <kushalkdl63@gmail.com>
2023-04-23 17:01:32 +0545

    added first java file

---
+++ variables_java.java
@@ -0,0 +1,12 @@
+class variables_java{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int sum;
+        sum = a + b;
+        System.out.print("the sum is: ");
+        System.out.println(sum);
+
+         
+    }
+}
