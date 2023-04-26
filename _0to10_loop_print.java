bf5c3f6bd9f4fb055bc03b4956a5ee2d6df7c361
kusal6199 <kushalkdl63@gmail.com>
2023-04-26 22:20:29 +0545

    for loop to print from 0 to 10 in java

---
+++ _0to10_loop_print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class _0to10_loop_print{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<=10;i++){
+        System.out.println(i);
+        // System.out.print(i);
+        // System.out.print("\n");
+        //both method can be applied;
+        }
+        sc.close();
+    }
+
+}
