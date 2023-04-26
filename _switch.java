c5afda9a018d5b4065a84eb9706bfa04b1e301c1
kusal6199 <kushalkdl63@gmail.com>
2023-04-26 21:57:22 +0545

    switch in java

---
+++ _switch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class _switch {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter number from 1-3 only: ");
+        int num=sc.nextInt();
+        switch(num){
+            case 1: System.out.println("sunday");
+            break;
+            case 2: System.out.println("monday");
+            break;
+            case 3: System.out.println("tuesday");
+            break;
+            default:  System.out.println("not valid number");
+        }
+        
+    }
+    
+}
