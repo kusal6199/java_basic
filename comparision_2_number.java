04b6010bda90bceb3c738ec15b1dbb0af33ef82d
kusal6199 <kushalkdl63@gmail.com>
2023-04-26 21:26:30 +0545

    added java file to compare two number

---
+++ comparision_2_number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class comparision_2_number {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st number: ");
+        int a=sc.nextInt();
+        System.out.println("enter 2nd number: ");
+        int b=sc.nextInt();
+        if(a==b){
+            System.out.println("both are equal");
+        }
+        else if(a>b){
+            System.out.print(a);
+            System.out.print(" is grater");
+        }
+        else {
+            System.out.print(b);
+            System.out.print(" is grater");
+        }
+
+
+
+    }
+    
+}
