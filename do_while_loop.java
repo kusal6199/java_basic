5e70c1adc54526fe567e70c0beeb07494b2eaa8b
kusal6199 <kushalkdl63@gmail.com>
2023-04-27 09:41:13 +0545

    java file that illustrate the do while loop

---
+++ do_while_loop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class do_while_loop {
+    public static void main(String args[]){
+     Scanner kushal=new Scanner(System.in);
+     int i=0;
+     int num;
+     System.out.print("enter the limit number: ");
+     num=kushal.nextInt();
+     do{
+        System.out.println(i);
+        i++;
+     } while(i<=num);
+
+     kushal.close();
+    }
+}
