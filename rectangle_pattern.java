0b33612a15b112f5c833d53e361e4c74255c5f8c
kusal6199 <kushalkdl63@gmail.com>
2023-04-27 14:17:02 +0545

    added java file to print the star pattern of star

---
+++ rectangle_pattern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class rectangle_pattern {
+    public static void main(String args[]){
+      Scanner sc =new Scanner(System.in);
+      int i,j;
+      for(i=1;i<=4;i++){
+        for(j=1;j<=5;j++){
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+      }
+      sc.close();
+    }
+    
+}
