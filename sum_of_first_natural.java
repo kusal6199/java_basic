c21495ae4cf03a1269c29dc640e60ab9322845b7
kusal6199 <kushalkdl63@gmail.com>
2023-04-27 09:57:33 +0545

    sum of n natural number using java

---
+++ sum_of_first_natural.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum_of_first_natural {
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in); 
+     int i,sum=0,num;  
+     System.out.print("enter the last limit natural num: ");  
+     num=sc.nextInt();
+     for(i=1;i<=num;i++){
+        sum+=i;
+     }
+     System.out.println("final sum: ");
+     System.out.println(sum);
+     sc.close();
+    }
+}
