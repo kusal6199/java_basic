96780f2654e291fcc48d8580aa9dc2bd9530d930
kusal6199 <kushalkdl63@gmail.com>
2023-04-27 16:06:42 +0545

    added java file that add two numbers using user define function

---
+++ function_inJAVA.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class function_inJAVA {
+    public static int calculateSum(int a,int b){
+        int sum=a+b;
+        return sum;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int a,b;
+        System.out.print("enter first number: ");
+        a=sc.nextInt();
+        System.out.print("enter second number: ");
+        b=sc.nextInt();
+        int c=calculateSum(a,b);
+        System.out.print("the sum is: "+c);
+        // System.out.print(c);
+
+        sc.close();
+
+    }
+}
