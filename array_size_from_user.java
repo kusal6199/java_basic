a92d1f8838330005546df0aeac2714c105da4fb2
kusal6199 <kushalkdl63@gmail.com>
2023-04-29 16:45:17 +0545

    added java file that take array size from user

---
+++ array_size_from_user.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class array_size_from_user {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n,i;
+        System.out.print("enter array size: ");
+        n=sc.nextInt();
+        int[] marks = new int[n];
+        System.out.println("enter marks of different subjects: ");
+        for(i=0;i<n;i++)
+        marks[i]=sc.nextInt();
+
+        System.out.println("marks of different subjects: ");
+        for(i=0;i<n;i++)
+        System.out.println(marks[i]);;
+        sc.close();
+    
+    }
+}
