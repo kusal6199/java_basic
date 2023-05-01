ab1c244c4a24c18aa5b771a306575ae3cd5e2ece
kusal6199 <kushalkdl63@gmail.com>
2023-05-01 22:01:24 +0545

    fixed error

--- substring.java
+++ substring.java
@@ -8,6 +8,7 @@ public class substring{
         //substring vanako string ko kunai part
         String part=sentence.substring(11,sentence.length());
         System.out.println(part);
+        //string_name.substring(beginingIndex:11,endingIndex:16)---> return string part begining from index 11 and upto index 15 only index 16 chracter is excluded
         
     
         sc.close();
