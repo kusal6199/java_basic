94931d8d1d4d775c966e4fa89a205d178c2019a0
kusal6199 <kushalkdl63@gmail.com>
2023-05-01 21:08:22 +0545

    added java file to show string length function

---
+++ string_length.java
@@ -0,0 +1,8 @@
+public class string_length {
+    public static void main(String args[]){
+        String firstName="Kushal";
+        String lastName="Kandel";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName.length());
+    }
+}
