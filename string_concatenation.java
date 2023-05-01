4b3e95fcc6602a38c3881623b33efcf038b0399d
kusal6199 <kushalkdl63@gmail.com>
2023-05-01 21:00:18 +0545

    added java file to show string concidation

---
+++ string_concatenation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class string_concatenation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String firstName="kushal";
+        String lastName="kandel";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName);
+
+        sc.close();
+
+    }
+    
+}
