31605d659c2ff7b784f995ef6258bab0479beb4f
kusal6199 <kushalkdl63@gmail.com>
2023-05-03 21:18:09 +0545

    added java file to find factorial using recursion

---
+++ factorial_recursion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class factorial_recursion {
+    public static int factorial(int n){
+        if (n==0||n==1) {
+            return n;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("enter a number for factorial: ");
+        n=sc.nextInt();
+        System.out.println("The factorial of "+n+" is "+ factorial(n));
+
+        sc.close();
+    }
+    
+}
