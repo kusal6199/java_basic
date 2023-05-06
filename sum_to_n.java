2d1867d802a133ffcc4ecef8896336909415dab2
kusal6199 <kushalkdl63@gmail.com>
2023-05-06 21:32:55 +0545

    added java file to find sum of n natural number using recursion

---
+++ sum_to_n.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class sum_to_n {
+    public static int sum(int n){
+        int sum;
+        if (n==0||n==1) {
+            return n;
+        }
+        else{
+            return sum=n+sum(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("enter the number upto which u want sum: ");
+        n=sc.nextInt();
+        System.out.println("the final sum is "+sum(n));
+        
+        sc.close();
+    }
+    
+}
