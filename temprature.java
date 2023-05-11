9f330b3eac715aff466037f32e472cc7881dc0a5
kusal6199 <kushalkdl63@gmail.com>
2023-05-11 19:25:12 +0545

    added java file to convert temprature value

---
+++ temprature.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class temprature {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the temprature at ferrnheit");
+        double fern = sc.nextDouble();
+        double cel =(100*(fern-32))/180;
+        System.out.println(fern+"ferneight is equal to "+cel+" celcious");
+        sc.close();
+    }
+    
+}
---
+++ testerror.java
@@ -0,0 +1,13 @@
+public class testerror {
+    public static void main(String[] args) {
+        int x=20;
+        char gender='m';
+        double salary=85830.45;
+        String mycollege ="Orchid International";
+        System.out.println();
+        System.out.print("x= "+x+" mycollege="+mycollege);
+        
+    
+    }
+
+}
